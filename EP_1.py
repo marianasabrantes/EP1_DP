612d8f5681a1c584162fba3ffde88aa87fabb264
marianasabrantes <38562078+marianasabrantes@users.noreply.github.com>
2018-09-11 17:02:52 -0300

    Menu e opção 1 feitas

--- EP_1.py
+++ EP_1.py
@@ -7,12 +7,23 @@ Created on Tue Sep 11 16:46:14 2018
 
 a = True
 while a:
+    cardapio = ['banana']
     print('0 - sair')
     print('1 - imprimir cardápio')
     print('2 - adicionar item')
     print('3 - remover item')
     print('4 - imprimir comanda')
     
-    int(input('Digite uma escolha: '))
-    
-    
+    escolha = int(input('Digite uma escolha: '))
+    if escolha == 0:
+        a = False
+        print ('Até mais!')
+    elif escolha == 1:
+        print('O cardápio possui os seguintes itens: {0}'.format(cardapio))
+    elif escolha == 2:
+        nome = input('Digite o nome do produto: ')
+        quant = int(input('Digite a quantidade do produto: '))
+        if nome in cardapio:
+            
+        else:
+            print('Produto não disponivel')
