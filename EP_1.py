20533e91984286d87568bb3a180d0dfbed1fc90b
marianasabrantes <38562078+marianasabrantes@users.noreply.github.com>
2018-09-11 16:52:48 -0300

    Menu do programa

---
+++ EP_1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 11 16:46:14 2018
+
+@author: Mariana Abrantes
+"""
+
+a = True
+while a:
+    print('0 - sair')
+    print('1 - imprimir cardápio')
+    print('2 - adicionar item')
+    print('3 - remover item')
+    print('4 - imprimir comanda')
+    
+    int(input('Digite uma escolha: '))
+    
+    
